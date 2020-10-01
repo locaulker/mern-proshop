6e6d7734224b12455f8c61ce3357dee5dac5b530
Lawrence <Caulker>
2020-10-01 09:02:10 -0400

    updated prics tags.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -38,7 +38,7 @@ const ProductScreen = ({ match }) => {
                 <Row>
                   <Col>Price:</Col>
                   <Col>
-                    <strong>{product.price}</strong>
+                    <strong>${product.price}</strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
